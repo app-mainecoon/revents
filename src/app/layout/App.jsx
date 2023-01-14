637e0823cdc21d3f1c3d79194496d53a0ee912d3
app-mainecoon <app.mainecoon@gmail.com>
2023-01-14 21:07:25 +0100

    Changes to App  - just for play

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Re-vents</h1>
-    </div>
-  );
+  const retValue = <div className="App">
+                <h1>Re-vents updated</h1>
+              </div>
+  console.log(retValue);
+  return (retValue );
 }
 
 export default App;
